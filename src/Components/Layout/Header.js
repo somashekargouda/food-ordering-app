0780ef8ab88abe76f82509d79d94c408d3bcd0d0
somashekar gouda <somashekar.patil09@gmail.com>
2021-09-16 00:56:58 +0530

    Add files via upload

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logoImage from "../../assets/logo.png";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+export default function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <img src={logoImage} className={classes.logo} />
+
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food !" />
+      </div>
+    </React.Fragment>
+  );
+}
